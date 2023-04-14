587fbd47b563c5b65f5f305664076dbb58867eb7
Yuki Abe <12268087+yukia3e@users.noreply.github.com>
2023-04-14 19:51:59 +0900

    Setup eslint and so on

--- frontends/ichimatsu-artists/tailwind.config.js
+++ frontends/ichimatsu-artists/tailwind.config.js
@@ -1,18 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/app/**/*.{js,ts,jsx,tsx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        primary: colors.gray,
+        accent: colors.gray,
+        alert: colors.red,
+        warn: colors.yellow,
+        success: colors.green,
       },
     },
   },
   plugins: [],
-}
+};
