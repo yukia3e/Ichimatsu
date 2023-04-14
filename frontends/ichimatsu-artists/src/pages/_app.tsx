d383338fff624b8bcfd5aa9ba746a4035e8090a9
Yuki Abe <12268087+yukia3e@users.noreply.github.com>
2023-04-14 20:09:48 +0900

    Remove unnecessary Next.js default files.

--- frontends/ichimatsu-artists/src/pages/_app.tsx
+++ frontends/ichimatsu-artists/src/pages/_app.tsx
@@ -1,6 +1,13 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import { ThirdwebProvider } from "@thirdweb-dev/react";
+import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps): JSX.Element => {
+  return (
+    <ThirdwebProvider activeChain="mumbai">
+      <Component {...pageProps} />
+    </ThirdwebProvider>
+  );
+};
+
+export default App;
